6fe26f2341379339483e34f5d4197422aa3f0f78
Daniel Ramirez <daniel@akosweb.com>
2018-08-09 23:32:37 -0700

    Adds support for ACF Pro options page

--- app/Setup/actions.php
+++ app/Setup/actions.php
@@ -23,3 +23,28 @@ function example_action()
     //
 }
 add_filter('excerpt_length', 'Tonik\Theme\App\Setup\example_action');
+
+/**
+ * Adds support for ACF Pro options page.
+ *
+ * @return void
+ */
+function add_acfpro_options_page()
+{
+    if ( function_exists('acf_add_options_page') ) {
+
+        /*$args = array(
+            'page_title' 	=> 'Theme General Settings',
+            'menu_title'	=> 'Theme Settings',
+            'menu_slug' 	=> 'theme-general-settings',
+            'capability'	=> 'edit_posts',
+            'redirect'		=> false
+        );
+
+        acf_add_options_page($args);
+
+        acf_add_options_page('Theme Settings');*/
+
+    }
+}
+add_action('acf/init', 'Tonik\Theme\App\Setup\add_acfpro_options_page');
